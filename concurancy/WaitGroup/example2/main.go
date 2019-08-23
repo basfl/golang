3ecf416373a7b138db2759cdf3d208695e4e015a
unknown <jupiter85838@yahoo.com>
2019-08-23 00:40:12 -0400

    adding example for waitGroup

---
+++ concurancy/WaitGroup/example2/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	urls := []string{
+		"https://www.google.com/",
+		"https://www.yahoo.com/",
+		"https://twitter.com/",
+	}
+	wg.Add(len(urls))
+	for _, url := range urls {
+		//wg.Add(1)
+		go fetch(url)
+	}
+	wg.Wait()
+	fmt.Println("*****")
+}
+func fetch(url string) {
+	_, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("requesting url= ", url)
+	wg.Done()
+	//fmt.Printf("%s", res)
+}
